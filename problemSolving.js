65d84913b90f327f0666776527f8f039b166d979
sucharita4867 <sucharitasardar7@gmail.com>
2026-01-07 22:15:19 +0530

    complete task 4 and 5

--- problemSolving.js
+++ problemSolving.js
@@ -1,35 +1,67 @@
 // problem 1
 // ----------------------------------
-let str = "hello";
-const reverse = str.split("").reverse().join("");
-// console.log(reverse);
+{
+  let str = "hello";
+  const reverse = str.split("").reverse().join("");
+  // console.log(reverse);
+}
 
 // problem 2
 // ----------------------------------
-let input = "programming";
-let count = 0;
-for (let i = 0; i < input.length; i++) {
-  //   console.log(input[i]);
-  if (
-    input[i] == "a" ||
-    input[i] == "e" ||
-    input[i] == "i" ||
-    input[i] == "o" ||
-    input[i] == "u"
-  ) {
-    count = count + 1;
+{
+  let input = "programming";
+  let count = 0;
+  for (let i = 0; i < input.length; i++) {
+    //   console.log(input[i]);
+    if (
+      input[i] == "a" ||
+      input[i] == "e" ||
+      input[i] == "i" ||
+      input[i] == "o" ||
+      input[i] == "u"
+    ) {
+      count = count + 1;
+    }
   }
+  // console.log(count);
 }
-// console.log(count);
 
 // problem 3
 // ----------------------------------
-
-const latter = "madam";
-const result = latter.split("").reverse().join("");
-if (latter === result) {
-  console.log("true");
-} else {
-  console.log("false");
+{
+  const latter = "madam";
+  const result = latter.split("").reverse().join("");
+  if (latter === result) {
+    //   console.log("true");
+  } else {
+    //   console.log("false");
+  }
+  // console.log(result);
 }
-// console.log(result);
+
+// problem 4
+// ----------------------------------
+{
+  const numbers = [5, 1, 9, 3, 29];
+  let num = numbers[0];
+  for (let i = 1; i < numbers.length; i++) {
+    if (numbers[i] > num) {
+      num = numbers[i];
+    }
+  }
+  //   console.log("max no: ", num);
+}
+
+// problem 4
+// ----------------------------------
+{
+  let input = [1, 2, 2, 3, 6, 5, 5, 4, 4];
+  const number = [];
+  for (let i = 0; i < input.length; i++) {
+    //   console.log(input[i]);
+    if (!number.includes(input[i])) {
+      number.push(input[i]);
+    }
+  }
+  console.log(number);
+}
