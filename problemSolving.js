ba5baca0d33489386be187c9c7c36b8e86aaff18
sucharita4867 <sucharitasardar7@gmail.com>
2026-01-07 20:55:31 +0530

    complete task 2

--- problemSolving.js
+++ problemSolving.js
@@ -2,4 +2,22 @@
 // ----------------------------------
 let str = "hello";
 const reverse = str.split("").reverse().join("");
-console.log(reverse);
+// console.log(reverse);
+
+// problem 2
+// ----------------------------------
+let input = "programming";
+let count = 0;
+for (let i = 0; i < input.length; i++) {
+//   console.log(input[i]);
+  if (
+    input[i] == "a" ||
+    input[i] == "e" ||
+    input[i] == "i" ||
+    input[i] == "o" ||
+    input[i] == "u"
+  ) {
+    count = count + 1;
+  }
+}
+console.log(count);
