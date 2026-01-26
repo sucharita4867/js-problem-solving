08e3e6e6209485740d577f9da2739a5c79c2c922
sucharita4867 <sucharitasardar7@gmail.com>
2026-01-26 14:52:25 +0530

    added this practice

--- practice.js
+++ practice.js
@@ -9,9 +9,23 @@ const num = [1, 2, 3, 4, 5, 6, 7, 8];
 let evenNo = [];
 for (let i = 0; i < num.length; i++) {
   if (num[i] % 2 === 0) {
-    console.log("even no:", num[i]);
+    //     console.log("even no:", num[i]);
     evenNo.push(num[i]);
   }
   //   else console.log("odd no", num[i]);
 }
-console.log(evenNo);
+// console.log(evenNo);
+
+const str = "hello";
+const reverse = str.split("").reverse().join("");
+// console.log(reverse);
+
+const duplicate = [1, 2, 2, 3, 4, 4, 5];
+let numbers = [];
+for (let i = 0; i < duplicate.length; i++) {
+  console.log(duplicate[i]);
+  if (!numbers.includes(duplicate[i])) {
+    numbers.push(duplicate[i]);
+  }
+}
+console.log(numbers);
