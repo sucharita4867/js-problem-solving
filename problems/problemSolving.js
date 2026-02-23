08f7af0202f1778b1237a1bb9c1c52f907f27aa5
sucharita4867 <sucharitasardar7@gmail.com>
2026-02-23 13:03:38 +0530

    open problem 2

--- problems/problemSolving.js
+++ problems/problemSolving.js
@@ -9,20 +9,20 @@
 // problem 2
 // ----------------------------------
 {
-  // let input = "programming";
-  // let count = 0;
-  // for (let i = 0; i < input.length; i++) {
-  //   if (
-  //     input[i] == "a" ||
-  //     input[i] == "e" ||
-  //     input[i] == "i" ||
-  //     input[i] == "o" ||
-  //     input[i] == "u"
-  //   ) {
-  //     count = count + 1;
-  //   }
-  // }
-  // console.log(count);
+  let input = "programming";
+  let count = 0;
+  for (let i = 0; i < input.length; i++) {
+    if (
+      input[i] == "a" ||
+      input[i] == "e" ||
+      input[i] == "i" ||
+      input[i] == "o" ||
+      input[i] == "u"
+    ) {
+      count = count + 1;
+    }
+  }
+  console.log(count);
 }
 
 // problem 3
